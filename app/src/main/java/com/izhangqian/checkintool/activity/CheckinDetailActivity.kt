b0f94adb6728f00842577f12a94f01ee84c3b198
xtayfjtn <xtayfjtn@yeah.net>
2020-07-12 21:46:48 +0800

    增加每步命令展示布局

--- app/src/main/java/com/izhangqian/checkintool/activity/CheckinDetailActivity.kt
+++ app/src/main/java/com/izhangqian/checkintool/activity/CheckinDetailActivity.kt
@@ -2,7 +2,10 @@ package com.izhangqian.checkintool.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.izhangqian.checkintool.R
+import com.izhangqian.checkintool.adapter.CommondListAdapter
+import com.izhangqian.checkintool.bean.checkin.CheckinCommond
 import com.izhangqian.checkintool.bean.checkin.CheckinMainBean
 import com.izhangqian.checkintool.utils.Constants
 import kotlinx.android.synthetic.main.activity_checkin_detail.*
@@ -11,24 +14,31 @@ class CheckinDetailActivity : AppCompatActivity() {
     var mCmdId : String = ""
     var mCmdName : String = ""
     var mCmdPack : String = ""
+    var mCmdList = mutableListOf<CheckinCommond>()
+    var mCheckinMainBean : CheckinMainBean? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkin_detail)
         initData()
         initView()
+        initEvent()
     }
 
     private fun initData() {
-        var checkinMainBean = intent.getParcelableExtra<CheckinMainBean>(Constants.EXTRA_CMD_BEAN)
-        if (checkinMainBean != null) {
-            mCmdId = checkinMainBean.cmdId
-            mCmdName = checkinMainBean.cmdName
-            mCmdPack = checkinMainBean.cmdpack
-        }
+        mCheckinMainBean = intent.getParcelableExtra<CheckinMainBean>(Constants.EXTRA_CMD_BEAN)
     }
 
     private fun initView() {
-        check_in_detail_name_et.setText(this.mCmdName)
-        check_in_detail_pack_et.setText(this.mCmdPack)
+        check_in_detail_name_et.setText(mCheckinMainBean?.cmdName)
+        check_in_detail_pack_et.setText(mCheckinMainBean?.cmdpack)
+        var linearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+        check_in_cmd_rv.layoutManager = linearLayoutManager
+        var commondListAdapter = CommondListAdapter(this, mCheckinMainBean?.cmdList)
+        check_in_cmd_rv.adapter = commondListAdapter
+    }
+
+    private fun initEvent() {
+
     }
 }
---
+++ app/src/main/java/com/izhangqian/checkintool/adapter/CommondListAdapter.kt
@@ -0,0 +1,42 @@
+package com.izhangqian.checkintool.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Spinner
+import androidx.recyclerview.widget.RecyclerView
+import com.izhangqian.checkintool.R
+import com.izhangqian.checkintool.bean.checkin.CheckinCommond
+
+class CommondListAdapter(context: Context) : RecyclerView.Adapter<CommondListAdapter.CommondViewHolder>() {
+
+    constructor(context: Context, list: MutableList<CheckinCommond>?) : this(context) {
+        if (list != null) {
+            mCmdList = list
+        }
+    }
+    var mContext = context
+    var mCmdList = mutableListOf<CheckinCommond>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommondViewHolder {
+
+        var view = LayoutInflater.from(mContext).inflate(R.layout.cmd_item_detail, parent, false)
+        return CommondViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return mCmdList.size
+    }
+
+    override fun onBindViewHolder(holder: CommondViewHolder, position: Int) {
+        holder.bindView(mCmdList[position])
+    }
+
+    class CommondViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val cmdSpinner = itemView.findViewById<Spinner>(R.id.cmd_step_type_select)
+        fun bindView(checkinCommond: CheckinCommond) {
+        }
+    }
+}
